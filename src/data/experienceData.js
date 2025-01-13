058edf5371f4cfcac8a53a76c7370c5eff9b6538
Monica Uwalaka <monicauwalaka@Monicas-Laptop.local>
2025-01-12 21:26:43 -0700

    fixed experience section

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,7 +1,7 @@
 const data = {
     jobs:[
-        {company:"Round Signal", role:"Software Engineer I", start:"Mar. 2022", end:"Present" },
-        {company:"Awele Technologies Inc.", role:"Software Developer Intern", start:"Jun 2021", end:"Aug 2021" },
+        {company:"Round Signal", role:"Software Developer", start:"August 2023", end:"September 2024" },
+        {company:"Awele Technologies Inc.", role:"Software Developer Intern", start:"June 2022", end:"July 2023" },
         {company:"CS Levelup Education Services Inc.", start:"Jun 2021", end:"Aug 2021"},
     ],
 }
